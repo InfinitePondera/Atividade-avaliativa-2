fe0f8522f5810bfb5663d36ca1a2332674eef299
Zyon <zyonamadeus01@gmail.com>
2021-12-08 21:27:51 -0300

    Commit #2

--- routes.js
+++ routes.js
@@ -1,19 +1,49 @@
 const express = require('express');
 const router = express.Router();
-const storage = require('node-persist')
+const storage = require('node-persist');
 
 const noticias = storage.create({dir:'noticias', ttl: 3000});
 const emails = storage.create({dir:'emails', ttl: 3000});
 
 await storage.init();
 
-router.get('/noticia', (req, res) => {
-    return res.status(200).send(
-        console.log(await noticias.values())
-    );
+router.get('/noticia', async (req, res) => {
+    const noticias = await noticias.getItem('noticias');
+    return res.status(200).send(noticias);
 });
-router.post('/noticia', (req, res) => {
-    await noticias.setItem('noticias', req.body);
+router.post('/noticia', async (req, res) => {
+    if (await noticias.getItem('noticias') != undefined) {
+        const noticias = await noticias.getItem('noticias');
+
+        noticias.push(req.body);
+
+        await noticias.setItem('noticias', noticias);
+    } else { 
+        return res.status(400).send('erro');
+    }
     return res.status(200).send('cadastrado');
+});
+router.get('/noticia/:id', async (req, res) => {
+    var id = parseInt(req.params.id);
+    const noticias = await noticias.getItem('noticias');
+
+    if (noticias === undefined){
+        return res.send(400);
+    } else {
+        const noticia = noticias.find(noticia => noticia.id === id);
+        return res.status(200).send(noticia);
+    }    
+});
+router.post('/inscricao', async (req, res) => {
+    if (await emails.getItem('emails') != undefined) {
+        const emails = await emails.getItem('emails');
+
+        emails.push(req.body);
+
+        await emails.setItem('email', emails);
+    } else {
+        return res.status(400).send('erro');
+    }
+    return res.status(200).send('success');
 })
 module.exports = router;
