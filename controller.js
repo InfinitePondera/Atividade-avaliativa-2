986ff1fbbfa9e8454fcabd7e7d330ff6dd336dc9
Zyon <zyonamadeus01@gmail.com>
2021-12-08 22:43:53 -0300

    Commit #4

--- controller.js
+++
@@ -1,9 +0,0 @@
-exports.post = (req, res, next) => {
-    res.status(200).send('Request recebido');
-};
-exports.put = (req, res, next) => {
-    res.status(200).send('Request enviado');
-};
-exports.get = (req, res, next) => {
-    res.status(200).send('Request recebido');
-}
--- server.js
+++ server.js
@@ -1,7 +1,1 @@
-const app = require('../src/app');
-const port = '8080';
-
-app.listen(port, function () {
-    console.log(`app listening on port ${port}`);
-});
 
